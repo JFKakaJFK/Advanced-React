3764e4b887c01178108389ec2e087d3a355edd25
JFKakaJFK <johannes.koch@live.at>
2019-02-01 17:51:17 +0100

    up to module 2 ex 6

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// done by next.js
+
+import Link from 'next/link' // change page without reload
+
+const Home = props => (
+  <div>
+    <p>Hey!</p>
+    
+  </div>
+);
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import Link from 'next/link' // change page without reload
+
+const Sell = props => (
+  <div>
+    <p>Sell!</p>
+    
+  </div>
+);
+
+export default Sell;
