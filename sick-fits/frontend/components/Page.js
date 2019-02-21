dcc44cf951fa60aad64756e8cfa0aa5e8c5023b6
JFKakaJFK <johannes.koch@live.at>
2019-02-21 13:00:39 +0100

    sync w/ notebook

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-import Header from '../components/Header';
-import Meta from '../components/Meta';
-
 // import styled from 'styled-components';
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
+
+import Header from '../components/Header';
+import Meta from '../components/Meta';
 
 const theme = {
   red: '#F00',
@@ -27,6 +27,35 @@ const Inner = styled.div`
   margin: 0 auto;
   padding: 2rem;
 `;
+
+injectGlobal`
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next', sans-serif;
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;  // theme.black works since def is in same folder
 
 class Page extends Component {
   render() {
